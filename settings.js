5f8d580c8038a5f00c50c6941709a0e86634750a
charindu12 <charindunuwan5@gmail.com>
2025-02-22 22:40:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Charindu',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "BLACK PANDA Bot",
+  botOwner: 'Charindu Nuwan', // Your name
+  ownerNumber: '94762890901', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
